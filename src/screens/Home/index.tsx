b72716663973e8c6bb77d53bfa6aa8b9f312ec5e
eloisa-santos <eloisa.santos@fcamara.com.br>
2023-06-27 11:34:45 -0300

    add button

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,6 +3,11 @@ import { Text, TextInput, View, TouchableOpacity } from 'react-native';
 import { styles } from './styles';
 
 export function Home() {
+
+    function handleParticipantAdd(){
+        console.log("Você clicou no botaão de Adicionar!");
+    }
+
     return (
         <View style={styles.container}>
             <Text style={styles.eventName}>
@@ -18,6 +23,12 @@ export function Home() {
                 placeholder='Nome do participante'
                 placeholderTextColor='#6B6B6B'
             />
+            
+            <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+                <Text style={styles.buttonText}>
+                    +
+                </Text>
+            </TouchableOpacity>
         </View>
     )
 }
