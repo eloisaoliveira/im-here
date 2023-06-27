5c0ab38bc9f38aa850b9e14a7b833255751ceb9f
eloisa-santos <eloisa.santos@fcamara.com.br>
2023-06-27 11:46:24 -0300

    add form

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -18,17 +18,19 @@ export function Home() {
                 Sexta, 30 de Junho de 2023
             </Text>
 
-            <TextInput
-                style={styles.input}
-                placeholder='Nome do participante'
-                placeholderTextColor='#6B6B6B'
-            />
+            <View style={styles.form}>
+                <TextInput
+                    style={styles.input}
+                    placeholder='Nome do participante'
+                    placeholderTextColor='#6B6B6B'
+                />
             
             <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
                 <Text style={styles.buttonText}>
                     +
                 </Text>
             </TouchableOpacity>
+            </View>
         </View>
     )
 }
